92334bd63c012fc288d8368983f526f48326619f
welann <wzcing@outlook.com>
2023-03-22 19:14:05 +0800

    将restart分支合并到master分支上

--- src/include/texture.h
+++ src/include/texture.h
@@ -1,45 +1,38 @@
-#include <glm/glm.hpp>
-#include "graphics.h"
+#include "../stb_image.h"
+#include "tgaimage.h"
 
-
-using namespace glm;
-
-enum class usage_t
+struct Color
 {
-    USAGE_LDR_COLOR,
-    USAGE_LDR_DATA,
-    USAGE_HDR_COLOR,
-    USAGE_HDR_DATA
+    float r, g, b, a;
+    Color();
+    Color(float R, float G, float B, float A = 255);
+    Color(const TGAColor& tga_color);
+
+    static float MAX_CHANNEL_VALUE;
+    static Color White;
+    static Color Red;
+    static Color Green;
+    static Color Blue;
+    static Color Black;
+
+    float& operator[](const size_t i);
+
+    float operator[](const size_t i) const;
+
+    Color operator+(const Color& color) const;
+
+    Color operator*(float intensity) const;
+
+    Color operator*(const Color& color) const;
 };
 
-struct texture_t
+struct material
 {
-    int   width, height;
-    vec4 *buffer;
-} ;
-
-struct cubemap_t
-{
-    texture_t *faces[6];
-} ;
-
-/* texture related functions */
-texture_t *texture_create(int width, int height);
-void       texture_release(texture_t *texture);
-texture_t *texture_from_file(const char *filename, usage_t usage);
-void       texture_from_colorbuffer(texture_t *texture, framebuffer_t *framebuffer);
-void       texture_from_depthbuffer(texture_t *texture, framebuffer_t *framebuffer);
-vec4       texture_repeat_sample(texture_t *texture, vec2 texcoord);
-vec4       texture_clamp_sample(texture_t *texture, vec2 texcoord);
-vec4       texture_sample(texture_t *texture, vec2 texcoord);
-
-/* cubemap related functions */
-cubemap_t *cubemap_from_files(const char *positive_x, const char *negative_x,
-                              const char *positive_y, const char *negative_y,
-                              const char *positive_z, const char *negative_z,
-                              usage_t usage);
-void       cubemap_release(cubemap_t *cubemap);
-vec4     cubemap_repeat_sample(cubemap_t *cubemap, vec3 direction);
-vec4     cubemap_clamp_sample(cubemap_t *cubemap, vec3 direction);
-vec4     cubemap_sample(cubemap_t *cubemap, vec3 direction);
-
+    TGAImage* diffuse_map;
+    TGAImage* normal_map;
+    TGAImage* specular_map;
+    Color     color;
+    Color     specular;
+    float     gloss;
+    float     bump_scale;
+};
